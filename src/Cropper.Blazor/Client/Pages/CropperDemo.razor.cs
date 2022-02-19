ef80e42e69c160aff91ff23685b0469150a030b7
MaxymGorn <maximus56132@gmail.com>
2022-02-20 01:20:56 +0200

    add demo events for cropper

--- src/Cropper.Blazor/Client/Pages/CropperDemo.razor.cs
+++ src/Cropper.Blazor/Client/Pages/CropperDemo.razor.cs
@@ -1,4 +1,10 @@
 ﻿using Cropper.Blazor.Base;
+using Cropper.Blazor.Events.CropEndEvent;
+using Cropper.Blazor.Events.CropEvent;
+using Cropper.Blazor.Events.CropMoveEvent;
+using Cropper.Blazor.Events.CropReadyEvent;
+using Cropper.Blazor.Events.CropStartEvent;
+using Cropper.Blazor.Events.ZoomEvent;
 using Cropper.Blazor.Models;
 using Cropper.Blazor.Services;
 using Microsoft.AspNetCore.Components;
@@ -20,6 +26,43 @@ namespace Cropper.Blazor.Client.Pages
                 ViewMode = ViewMode.Vm0,
                 //DragMode =  DragMode.Crop.ToString()
             };
+        }
+
+        public void OnCropEndEvent(CropEndEvent cropEndEvent)
+        {
+            Console.WriteLine($"CropEndEvent, {cropEndEvent.ActionEvent}");
+        }
+
+        public void OnCropStartEvent(CropStartEvent cropStartEvent)
+        {
+            Console.WriteLine($"CropStartEvent, {cropStartEvent.ActionEvent}");
+        }
+
+        public void OnZoomEvent(ZoomEvent zoomEvent)
+        {
+            Console.WriteLine($"ZoomEvent, OldRatio: {zoomEvent.OldRatio}, Ratio: {zoomEvent.Ratio}");
+        }
+
+        public void OnCropMoveEvent(CropMoveEvent cropMoveEvent)
+        {
+            Console.WriteLine($"CropMoveEvent, {cropMoveEvent.ActionEvent}");
+        }
+
+        public void OnCropReadyEvent(CropReadyEvent cropReadyEvent)
+        {
+            Console.WriteLine("Cropper Is Ready");
+        }
+
+        public void OnLoadImageEvent()
+        {
+            Console.WriteLine("Image Is loaded");
+        }
+
+        public void OnCropEvent(CropEvent cropEvent)
+        {
+            Console.WriteLine($"CropEvent, X: {cropEvent.X}, Y: {cropEvent.Y}, " +
+                $"Height: {cropEvent.Height}, Width: {cropEvent.Width}, " +
+                $"ScaleX: {cropEvent.ScaleX}, ScaleY: {cropEvent.ScaleY}, Rotate: {cropEvent.Rotate}");
         }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
