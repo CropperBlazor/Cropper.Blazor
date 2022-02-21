a9d4e348bbb0ed2d4685de30d93a274f8090d6e0
Max56132 <50423072+MaxymGorn@users.noreply.github.com>
2022-02-22 00:21:34 +0200

    Revert "Fix/init dynamic field cropper"

--- src/Cropper.Blazor/Client/Services/UserPreferences/UserPreferences.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Cropper.Blazor.Client.Services.UserPreferences;
-public class UserPreferences
-{
-    /// <summary>
-    /// If true DarkTheme is used. LightTheme otherwise
-    /// </summary>
-    public bool IsDarkMode { get; set; }
-}
---
+++ src/Cropper.Blazor/Cropper.Blazor/ExampleJsInterop.cs
@@ -0,0 +1,41 @@
+using Microsoft.JSInterop;
+
+namespace Cropper.Blazor
+{
+    // This class provides an example of how JavaScript functionality can be wrapped
+    // in a .NET class for easy consumption. The associated JavaScript module is
+    // loaded on demand when first needed.
+    //
+    // This class can be registered as scoped DI service and then injected into Blazor
+    // components for use.
+
+    public class ExampleJsInterop : IAsyncDisposable
+    {
+        private readonly IJSRuntime jsRuntime;
+        private IJSObjectReference module;
+
+        public ExampleJsInterop(IJSRuntime jsRuntime)
+        {
+            this.jsRuntime = jsRuntime;
+        }
+
+        public async Task LoadAsync()
+        {
+            module = await jsRuntime.InvokeAsync<IJSObjectReference>(
+                "import", "./_content/Cropper.Blazor/exampleJsInterop.js");
+        }
+
+        public async ValueTask<string> Prompt(string message)
+        {
+            return await module.InvokeAsync<string>("showPrompt", message);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (module!=null)
+            {
+                await module.DisposeAsync();
+            }
+        }
+    }
+}
