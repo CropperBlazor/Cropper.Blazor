b2b8df87c51dd34c70d33b8409c4927c630e2d01
Max56132 <50423072+MaxymGorn@users.noreply.github.com>
2022-12-11 19:35:34 +0200

    add blazor server, maui examples (#107)
    ## Target
    <!--
      Why are you making this change?
     -->
    add blazor server, maui examples
    #### Open Questions
    <!-- OPTIONAL
    - [ ] Use the GitHub checklists to spark discussion on issues that may
    arise from your approach. Please tick the box and explain your answer.
    -->
    
    ## Checklist
    <!--
    It serves as a gentle reminder for common tasks. Confirm it's done and
    check everything that applies.
    -->
    - [ ] Documentation updated
    - [x] Tests cover new or modified code
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] New dependencies added
    - [ ] Includes breaking changes
    - [ ] Version bumped
    
    ## Visuals
    <!-- OPTIONAL
    Show results both before and after this change. When the output changes,
    it can be a screenshot of a trace, metric, or log illustrating the
    change.
    -->

---
+++ examples/Cropper.Blazor.MAUI.Net6/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Cropper.Blazor.MAUI.Net6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ examples/Cropper.Blazor.MAUI.Net7/MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using Cropper.Blazor.Extensions;
+using Cropper.Blazor.MAUI.Net7.Data;
+using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
+
+namespace Cropper.Blazor.MAUI.Net7
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddCropper();
+            builder.Services.AddMudServices();
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            return builder.Build();
+        }
+    }
+}
