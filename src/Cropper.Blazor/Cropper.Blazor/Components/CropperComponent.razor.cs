042408d90cd144eddc2e6303bf03e5041e90c281
ColdForeign <tankon46555@gmail.com>
2022-12-26 18:22:00 +0200

    XML comments for 'CropperComponent.razor.cs' added

--- src/Cropper.Blazor/Cropper.Blazor/Components/CropperComponent.razor.cs
+++ src/Cropper.Blazor/Cropper.Blazor/Components/CropperComponent.razor.cs
@@ -19,27 +19,46 @@ using ErrorEventArgs = Microsoft.AspNetCore.Components.Web.ErrorEventArgs;
 
 namespace Cropper.Blazor.Components
 {
+    
     public partial class CropperComponent : ICropperComponentBase
     {
         [Inject] ICropperJsInterop CropperJsIntertop { get; set; } = null!;
 
         private ElementReference imageReference;
 
+        /// <summary>
+        /// The options for cropping. Check out the available <see cref="Models.Options"/>
+        /// </summary>
         [Parameter]
         public Options Options { get; set; } = null!;
 
+        /// <summary>
+        /// Specifies the path to the image.
+        /// </summary>
         [Parameter]
         public string Src { get; set; } = null!;
 
+        /// <summary>
+        /// Specifies the path to the image when loading from <see cref="Src"/> fails
+        /// </summary>
         [Parameter]
         public string ErrorLoadImageSrc { get; set; } = null!;
 
+        /// <summary>
+        /// User class names for error image, separated by space
+        /// </summary>
         [Parameter]
         public string ErrorLoadImageClass { get; set; } = null!;
 
+        /// <summary>
+        /// Returns the state of image loading
+        /// </summary>
         [Parameter]
         public bool IsErrorLoadImage { get; set; }
 
+        /// <summary>
+        /// User class names, separated by space
+        /// </summary>
         [Parameter]
         public string Class { get; set; } = null!;
 
@@ -133,101 +152,204 @@ namespace Cropper.Blazor.Components
             OnReadyEvent?.Invoke(cropReadyEvent);
         }
 
+        /// <summary>
+        /// Change the drag mode.
+        /// </summary>
+        /// <param name="dragMode"></param>
         public void SetDragMode(DragMode dragMode)
         {
             CropperJsIntertop?.SetDragModeAsync(dragMode).AsTask();
         }
 
+        /// <summary>
+        /// Zoom the canvas (image wrapper) with a relative ratio
+        /// </summary>
+        /// <param name="ratio">
+        /// Zoom in: requires a positive number (ratio &gt; 0). 
+        /// <br/>
+        /// Zoom out: requires a negative number (ratio &lt; 0)
+        /// </param>
         public void Zoom(decimal ratio)
         {
             CropperJsIntertop?.ZoomAsync(ratio).AsTask();
         }
 
+        /// <summary>
+        /// Zoom the canvas (image wrapper) to an absolute ratio
+        /// </summary>
+        /// <param name="ratio">Requires a positive number (ratio > 0)</param>
+        /// <param name="pivotX">The X coordinate of the center point for zooming, base on the top left corner of the cropper container.</param>
+        /// <param name="pivotY">The Y coordinate of the center point for zooming, base on the top left corner of the cropper container.</param>
         public void ZoomTo(decimal ratio, decimal pivotX, decimal pivotY)
         {
             CropperJsIntertop?.ZoomToAsync(ratio, pivotX, pivotY).AsTask();
         }
 
+        /// <summary>
+        /// Move the canvas (image wrapper) with relative offsets
+        /// </summary>
+        /// <param name="offsetX">Moving size (px) in the horizontal direction</param>
+        /// <param name="offsetY">Moving size (px) in the vertical direction. If not present, its default value is offsetX</param>
         public void Move(decimal offsetX, decimal? offsetY)
         {
             CropperJsIntertop?.MoveAsync(offsetX, offsetY).AsTask();
         }
 
+        /// <summary>
+        /// Move the canvas (image wrapper) to an absolute point
+        /// </summary>
+        /// <param name="x">The left value of the canvas</param>
+        /// <param name="y">The top value of the canvas. If not present, its default value is x</param>
         public void MoveTo(decimal x, decimal? y)
         {
             CropperJsIntertop?.MoveToAsync(x, y).AsTask();
         }
 
+        /// <summary>
+        /// Rotate the image to a relative degree
+        /// </summary>
+        /// <param name="degree"> 
+        /// Rotate right: requires a positive number (degree &gt; 0)
+        /// <br/>
+        /// Rotate left: requires a negative number (degree &lt; 0)
+        /// </param>
         public void Rotate(decimal degree)
         {
             CropperJsIntertop?.RotateAsync(degree).AsTask();
         }
 
+        /// <summary>
+        /// Scale the abscissa of the image
+        /// </summary>
+        /// <param name="scaleX"> 
+        /// The scaling factor applies to the abscissa of the image
+        /// <br/>
+        /// When equal to 1 (default value) it does nothing
+        /// </param>
         public void ScaleX(decimal scaleX)
         {
             CropperJsIntertop?.ScaleXAsync(scaleX).AsTask();
         }
 
+        /// <summary>
+        /// Scale the ordinate of the image
+        /// </summary>
+        /// <param name="scaleY">
+        /// The scaling factor to apply on the ordinate of the image
+        /// <br/>
+        /// When equal to 1 (default value) it does nothing
+        /// </param>
         public void ScaleY(decimal scaleY)
         {
             CropperJsIntertop?.ScaleYAsync(scaleY).AsTask();
         }
 
+        /// <summary>
+        /// Scale the image
+        /// </summary>
+        /// <param name="scaleX"> 
+        /// The scaling factor applies to the abscissa of the image
+        /// <br/>
+        /// When equal to 1 (default value) it does nothing
+        /// </param>
+        /// <param name="scaleY">
+        /// The scaling factor to apply on the ordinate of the image
+        /// <br/>
+        /// If not present, its default value is <paramref name="scaleX"/>
+        /// </param>
         public void Scale(decimal scaleX, decimal scaleY)
         {
             CropperJsIntertop?.ScaleAsync(scaleX, scaleY).AsTask();
         }
 
+        /// <summary>
+        /// Show the crop box manually
+        /// </summary>
         public void Crop()
         {
             CropperJsIntertop?.CropAsync().AsTask();
         }
 
+        /// <summary>
+        /// Clear the crop box
+        /// </summary>
         public void Clear()
         {
             CropperJsIntertop?.ClearAsync().AsTask();
         }
 
+        /// <summary>
+        /// Enable (unfreeze) the cropper
+        /// </summary>
         public void Enable()
         {
             CropperJsIntertop?.EnableAsync().AsTask();
         }
 
+        /// <summary>
+        /// Disable (freeze) the cropper
+        /// </summary>
         public void Disable()
         {
             CropperJsIntertop?.DisableAsync().AsTask();
         }
 
+        /// <summary>
+        /// Reset the image and crop box to its initial states
+        /// </summary>
         public void Reset()
         {
             CropperJsIntertop?.ResetAsync().AsTask();
         }
 
+        /// <summary>
+        /// Destroy the cropper and remove the instance from the image
+        /// </summary>
         public void Destroy()
         {
             CropperJsIntertop?.DestroyAsync().AsTask();
         }
 
+        /// <summary>
+        /// Change the aspect ratio of the crop box
+        /// </summary>
+        /// <param name="aspectRatio">Requires a positive number</param>
         public void SetAspectRatio(decimal aspectRatio)
         {
             CropperJsIntertop?.SetAspectRatioAsync(aspectRatio).AsTask();
         }
 
+        /// <summary>
+        /// Change the crop box position and size with new data
+        /// </summary>
+        /// <param name="cropBoxDataOptions"></param>
         public void SetCropBoxData(SetCropBoxDataOptions cropBoxDataOptions)
         {
             CropperJsIntertop?.SetCropBoxDataAsync(cropBoxDataOptions).AsTask();
         }
 
+        /// <summary>
+        /// Change the cropped area position and size with new data (based on the original image)
+        /// </summary>
+        /// <param name="setDataOptions"></param>
         public void SetData(SetDataOptions setDataOptions)
         {
             CropperJsIntertop?.SetDataAsync(setDataOptions).AsTask();
         }
 
+        /// <summary>
+        /// Change the canvas (image wrapper) position and size with new data
+        /// </summary>
+        /// <param name="setCanvasDataOptions"></param>
         public void SetCanvasData(SetCanvasDataOptions setCanvasDataOptions)
         {
             CropperJsIntertop?.SetCanvasDataAsync(setCanvasDataOptions).AsTask();
         }
 
+        /// <summary>
+        /// Output the crop box position and size data
+        /// </summary>
+        /// <returns></returns>
         public async ValueTask<CropBoxData> GetCropBoxDataAsync()
         {
             return await CropperJsIntertop!.GetCropBoxDataAsync().AsTask();
