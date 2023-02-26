b7ac1f1eb5f08953a5dff85a595014ad7639d20d
MaxymGorn <maximus56132@gmail.com>
2023-02-27 01:52:43 +0200

    add example application, fix pipeline, update packages

---
+++ examples/Cropper.MVC.With.Blazor.Server.Net7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cropper.MVC.With.Blazor.Server.Net7.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
