cc1ebf107ab6183af1c0b78c316ddb21ef3d64ea
ColdForeign <tankon46555@gmail.com>
2023-05-24 09:58:11 +0300

    DocsSectionLink added

---
+++ src/Cropper.Blazor/Client/Models/DocsSectionLink.cs
@@ -0,0 +1,8 @@
+﻿namespace Cropper.Blazor.Client.Models;
+
+public class DocsSectionLink
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public bool Active { get; set; }
+}
