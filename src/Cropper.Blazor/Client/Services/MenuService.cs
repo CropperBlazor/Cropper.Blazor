69a61e2ccfc4f0cc4e92c39c7bc061f32fa9c246
ColdForeign <tankon46555@gmail.com>
2023-06-16 10:07:22 +0300

    Menu Service updated

--- src/Cropper.Blazor/Client/Services/MenuService.cs
+++ src/Cropper.Blazor/Client/Services/MenuService.cs
@@ -21,7 +21,8 @@ namespace Cropper.Blazor.Client.Services
             {
                 new DocsLink {Title = "Basic Usage", Href = "examples/cropperusage"},
                 new DocsLink {Title = "Crop Box Dimensions", Href = "examples/dimensions"},
-                new DocsLink {Title = "View Modes", Href = "examples/viewmodes"}
-            }.OrderBy(x => x.Title);
+                new DocsLink {Title = "View Modes", Href = "examples/viewmodes"},
+                new DocsLink {Title = "Aspect Ratio", Href = "examples/aspectratio"}
+            };
     }
 }
