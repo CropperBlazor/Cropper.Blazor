f881a7214bf6fea73accc40873e0fad8d292cc07
biegehydra <connorhallman29@gmail.com>
2023-07-09 00:50:07 -0400

    Create DecodedDataUrl object

---
+++ src/Cropper.Blazor/Cropper.Blazor/Models/DecodedDataUrl.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Cropper.Blazor.Extensions;
+
+namespace Cropper.Blazor.Models
+{
+    /// <summary>
+    /// A class that represents a decoded data url.
+    /// </summary>
+    public class DecodedDataUrl
+    {
+        /// <summary>
+        /// The image data of the data url converted from Base64.
+        /// </summary>
+        public required byte[] ImageData { get; set; }
+
+        /// <summary>
+        /// The media type defined in the data url. (e.g. image/png;base64)
+        /// </summary>
+        public required string MediaType { get; set; }
+
+        /// <summary>
+        /// Decodes a data url into a DecodedDataUrl object.
+        /// </summary>
+        /// <param name="dataUrl"></param>
+        [SetsRequiredMembers]
+        public DecodedDataUrl(string dataUrl)
+        {
+            (ImageData, MediaType) = DataUrlDecoder.Decode(dataUrl);
+        }
+    }
+}
