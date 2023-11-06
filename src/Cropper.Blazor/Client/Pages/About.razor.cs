da58751ca88059c9c505bdff6b0c5a5262905f7f
Gornytskyi Maxim <50423072+MaxymGorn@users.noreply.github.com>
2023-11-06 18:39:57 +0200

    Add about (#255)
    ## Target
    <!--
      Why are you making this change?
     -->
    Add about
    #### Open Questions
    <!-- OPTIONAL
    - [ ] Use the GitHub checklists to spark discussion on issues that may
    arise from your approach. Please tick the box and explain your answer.
    -->
    
    ## Checklist
    <!--
    It serves as a gentle reminder for common tasks. Confirm it's done and
    check everything that applies.
    -->
    - [x] Documentation updated
    - [x] Tests cover new or modified code
    - [x] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] New dependencies added
    - [ ] Includes breaking changes
    - [ ] Version bumped
    
    ## Visuals
    <!-- OPTIONAL
    Show results both before and after this change. When the output changes,
    it can be a screenshot of a trace, metric, or log illustrating the
    change.
    -->

---
+++ src/Cropper.Blazor/Client/Pages/About.razor.cs
@@ -0,0 +1,31 @@
+﻿using Cropper.Blazor.Client.Models;
+
+namespace Cropper.Blazor.Client.Pages
+{
+    public partial class About
+    {
+        public IEnumerable<TeamMember> TeamMembers => new TeamMember[]
+        {
+            new TeamMember
+            {
+                Avatar = "https://avatars.githubusercontent.com/u/50423072?v=4",
+                From = "Kyiv, Ukraine",
+                Github = "https://github.com/MaxymGorn",
+                LinkedIn = "https://www.linkedin.com/in/maxym-gornytskiy/",
+                Name = "Maksym Hornytskiy",
+                Role = "Creator",
+                Languages = "English, Ukrainian"
+            },
+            new TeamMember
+            {
+                Avatar = "https://avatars.githubusercontent.com/u/38187349?v=4",
+                From = "Ukraine",
+                Github = "https://github.com/ColdForeign",
+                LinkedIn = string.Empty,
+                Name = "George Radchuk",
+                Role = "Creator",
+                Languages = "English, Ukrainian"
+            }
+        };
+    }
+}
