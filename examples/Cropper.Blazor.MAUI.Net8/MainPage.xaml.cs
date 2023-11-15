27a63fc9c51399886af4bf75791fbf5932617da6
MaxymGorn <maximus56132@gmail.com>
2023-11-15 16:47:19 +0200

    add .net 8 for library

---
+++ examples/Cropper.Blazor.MAUI.Net8/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Cropper.Blazor.MAUI.Net8
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ examples/Cropper.Blazor.MAUI.Net8/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Cropper.Blazor.MAUI.Net8
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
