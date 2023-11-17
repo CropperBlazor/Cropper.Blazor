846f328a40cf9df84e3ff74b8aba894d5ce95784
Gornytskyi Maxim <50423072+MaxymGorn@users.noreply.github.com>
2023-11-17 15:58:58 +0200

    add .net 8 support (#266)
    ## Target
    <!--
      Why are you making this change?
     -->
    Add .net 8 support
    #### Open Questions
    <!-- OPTIONAL
    - [ ] Use the GitHub checklists to spark discussion on issues that may
    arise from your approach. Please tick the box and explain your answer.
    -->
    
    ## Checklist
    <!--
    It serves as a gentle reminder for common tasks. Confirm it's done and
    check everything that applies.
    -->
    - [x] Documentation updated
    - [x] Tests cover new or modified code
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] New dependencies added
    - [ ] Includes breaking changes
    - [x] Version bumped
    
    ## Visuals
    <!-- OPTIONAL
    Show results both before and after this change. When the output changes,
    it can be a screenshot of a trace, metric, or log illustrating the
    change.
    -->

---
+++ examples/Cropper.Blazor.MAUI.Net8/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Cropper.Blazor.MAUI.Net8
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
