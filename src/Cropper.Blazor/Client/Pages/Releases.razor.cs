6fa0f0d82d83149cee709666f5cda1d46931fc79
Gornytskyi Maxim <50423072+MaxymGorn@users.noreply.github.com>
2023-11-20 01:43:22 +0200

    Feature/add releases (#271)
    ## Target
    <!--
      Why are you making this change?
     -->
    Add releases
    #### Open Questions
    <!-- OPTIONAL
    - [ ] Use the GitHub checklists to spark discussion on issues that may
    arise from your approach. Please tick the box and explain your answer.
    -->
    
    ## Checklist
    <!--
    It serves as a gentle reminder for common tasks. Confirm it's done and
    check everything that applies.
    -->
    - [x] Documentation updated
    - [ ] Tests cover new or modified code
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] New dependencies added
    - [ ] Includes breaking changes
    - [ ] Version bumped
    
    ## Visuals
    <!-- OPTIONAL
    Show results both before and after this change. When the output changes,
    it can be a screenshot of a trace, metric, or log illustrating the
    change.
    -->

---
+++ src/Cropper.Blazor/Client/Pages/Releases.razor.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.RegularExpressions;
+using Cropper.Blazor.Client.Models;
+using Cropper.Blazor.Client.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace Cropper.Blazor.Client.Pages
+{
+    public partial class Releases : IDisposable
+    {
+        [Inject]
+        public GitHubApiClient GitHubApiClient { get; set; } = null!;
+
+        private GitHubReleases[] _githubReleases = Array.Empty<GitHubReleases>();
+        private CancellationToken _cancellationToken;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _cancellationToken = new();
+            _githubReleases = await GitHubApiClient.GetReleasesAsync(_cancellationToken);
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            _cancellationToken.ThrowIfCancellationRequested();
+        }
+
+        private string GetBody(string value)
+        {
+            value = Regex.Replace(value, @"((###\s)(?<subtitle>.+))(?<items>.*(?:\r?\n[*] .*)*)", "<h6 class=\"mud-typography mud-typography-h6\">${subtitle}</h6><ul class=\"mt-3 mb-6 px-6\">${items}</ul>");
+            value = Regex.Replace(value, @"((##\s)(?<title>.+))(?<items>.*(?:\r?\n[*] .*)*)", "<h5 class=\"mud-typography mud-typography-h5\">${title}</h5><ul class=\"mt-3 mb-6 px-6\">${items}</ul>");
+            value = Regex.Replace(value, @"-\s*(.*)", "<li>$1</li>", RegexOptions.Multiline);
+            value = Regex.Replace(value, @"(@)(\S+)", "<a target=\"_blank\" href=\"https://github.com/$2\" class=\"mud-link mud-default-text mud-link-underline-hover\"><b>@$2</b></a>");
+            value = Regex.Replace(value, @"https://github.com/CropperBlazor/Cropper.Blazor/pull/([0-9]{4})", "<a rel=\"noopener\" style=\"color: var(--mud-palette-primary); target=\"_blank\" href=\"\" class=\"mud-link mud-primary-text mud-link-underline-hover\"><b>#$1</b></a>");
+            value = Regex.Replace(value, @"([*][*]Full Changelog[*][*]: )(https://github.com/CropperBlazor/Cropper.Blazor/compare/)(.+)", "<p class=\"mud-typography mud-typography-body1\">Full Changelog: <a rel=\"noopener\" style=\"color: var(--mud-palette-primary); target=\"_blank\" href=\"$2$3\" class=\"docs-code docs-code-primary\">$3</a></p>");
+            value = Regex.Replace(value, @"\[([^)]+)\]\(([^)]+)\)", $"<a class=\"text-with-dots\" target=\"_blank\" rel=\"noopener\" style=\"color: var(--mud-palette-primary);\" href=\"$2\">$1</a>");
+            value = Regex.Replace(value, @"\(([^)]+)\)", ReplaceUrl, RegexOptions.IgnoreCase);
+
+            return value;
+        }
+
+        private string ReplaceUrl(Match match)
+        {
+            // Get the URL from the match
+            string url = match.Groups[1].Value;
+
+            // Check if the URL is valid
+            if (Uri.TryCreate(url, UriKind.Absolute, out Uri result))
+            {
+                // If valid, return the replacement string
+                return $"(<a target=\"_blank\" class=\"text-with-dots\" rel=\"noopener\" style=\"color: var(--mud-palette-primary);\" href=\"{result}\">{result}</a>)";
+            }
+            else
+            {
+                // If invalid, return the original string
+                return match.Value;
+            }
+        }
+    }
+}
