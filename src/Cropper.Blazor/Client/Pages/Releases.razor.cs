63961ccfef222ab10dbdc72f3e76cb4f15e2d759
MaxymGorn <maximus56132@gmail.com>
2023-12-05 12:47:04 +0200

    fix desc

--- src/Cropper.Blazor/Client/Pages/Releases.razor.cs
+++ src/Cropper.Blazor/Client/Pages/Releases.razor.cs
@@ -2,6 +2,8 @@
 using Cropper.Blazor.Client.Models;
 using Cropper.Blazor.Client.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.FileSystemGlobbing.Internal;
+using static MudBlazor.Colors;
 
 namespace Cropper.Blazor.Client.Pages
 {
@@ -31,15 +33,14 @@ namespace Cropper.Blazor.Client.Pages
             value = Regex.Replace(value, @"((##\s)(?<title>.+))(?<items>.*(?:\r?\n[*] .*)*)", "<h5 class=\"mud-typography mud-typography-h5\">${title}</h5><ul class=\"mt-3 mb-6 px-6\">${items}</ul>");
             value = Regex.Replace(value, @"-\s*(.*)", "<li>$1</li>", RegexOptions.Multiline);
             value = Regex.Replace(value, @"(@)(\S+)", "<a target=\"_blank\" href=\"https://github.com/$2\" class=\"mud-link mud-default-text mud-link-underline-hover\"><b>@$2</b></a>");
-            value = Regex.Replace(value, @"https://github.com/CropperBlazor/Cropper.Blazor/pull/([0-9]{4})", "<a rel=\"noopener\" style=\"color: var(--mud-palette-primary); target=\"_blank\" href=\"\" class=\"mud-link mud-primary-text mud-link-underline-hover\"><b>#$1</b></a>");
             value = Regex.Replace(value, @"([*][*]Full Changelog[*][*]: )(https://github.com/CropperBlazor/Cropper.Blazor/compare/)(.+)", "<p class=\"mud-typography mud-typography-body1\">Full Changelog: <a rel=\"noopener\" style=\"color: var(--mud-palette-primary); target=\"_blank\" href=\"$2$3\" class=\"docs-code docs-code-primary\">$3</a></p>");
             value = Regex.Replace(value, @"\[([^)]+)\]\(([^)]+)\)", $"<a class=\"text-with-dots\" target=\"_blank\" rel=\"noopener\" style=\"color: var(--mud-palette-primary);\" href=\"$2\">$1</a>");
-            value = Regex.Replace(value, @"\(([^)]+)\)", ReplaceUrl, RegexOptions.IgnoreCase);
+            value = Regex.Replace(value, @"\(([^)]+)\)", ReplaceLinkUrl, RegexOptions.IgnoreCase);
 
             return value;
         }
 
-        private string ReplaceUrl(Match match)
+        private string ReplaceLinkUrl(Match match)
         {
             // Get the URL from the match
             string url = match.Groups[1].Value;
@@ -47,8 +48,17 @@ namespace Cropper.Blazor.Client.Pages
             // Check if the URL is valid
             if (Uri.TryCreate(url, UriKind.Absolute, out Uri result))
             {
-                // If valid, return the replacement string
-                return $"(<a target=\"_blank\" class=\"text-with-dots\" rel=\"noopener\" style=\"color: var(--mud-palette-primary);\" href=\"{result}\">{result}</a>)";
+                string newUrl = url;
+                newUrl = Regex.Replace(newUrl, @"https:\/\/github\.com\/([^\/]+)\/([^\/]+)\/pull\/(\d+)", $"<a target=\"_blank\" class=\"text-with-dots\" rel=\"noopener\" style=\"color: var(--mud-palette-primary);\" href=\"{url}\">PR#$3</a>");
+                newUrl = Regex.Replace(newUrl, @"https:\/\/github\.com\/([^\/]+)\/([^\/]+)\/issues\/(\d+)", $"<a target=\"_blank\" class=\"text-with-dots\" rel=\"noopener\" style=\"color: var(--mud-palette-primary);\" href=\"{url}\">Issue#$3</a>");
+
+                if (newUrl == url)
+                {
+                    // If valid, return the replacement string
+                    return $"(<a target=\"_blank\" class=\"text-with-dots\" rel=\"noopener\" style=\"color: var(--mud-palette-primary);\" href=\"{result}\">{result}</a>)";
+                }
+
+                return newUrl;
             }
             else
             {
