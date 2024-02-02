68dfc6d6cb4043dc8f7c5ade6ec492dbf22974bd
Gornytskyi Maxim <50423072+MaxymGorn@users.noreply.github.com>
2024-02-02 20:02:38 +0200

    Update packages, add DataUrlDecoder.Decode extension method (#297)
    ## Target
    <!--
      Why are you making this change?
     -->
    Update packages, add DataUrlDecoder.Decode extension method
    #### Open Questions
    <!-- OPTIONAL
    - [ ] Use the GitHub checklists to spark discussion on issues that may
    arise from your approach. Please tick the box and explain your answer.
    -->
    
    ## Checklist
    <!--
    It serves as a gentle reminder for common tasks. Confirm it's done and
    check everything that applies.
    -->
    - [ ] Documentation updated
    - [x] Tests cover new or modified code
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] New dependencies added
    - [ ] Includes breaking changes
    - [x] Version bumped
    
    ## Visuals
    <!-- OPTIONAL
    Show results both before and after this change. When the output changes,
    it can be a screenshot of a trace, metric, or log illustrating the
    change.
    -->

---
+++ src/Cropper.Blazor/Cropper.Blazor/Extensions/DataUrlDecoder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Cropper.Blazor.Extensions
+{
+    /// <summary>
+    /// Class for decoding data URLs.
+    /// </summary>
+    public static class DataUrlDecoder
+    {
+        // lang=regex
+        private const string DataUrlPattern = @"data:(?<type>.+?),(?<data>.+)";
+        private static readonly Regex DataUrlRegex = new(DataUrlPattern, RegexOptions.Compiled);
+
+        /// <summary>
+        /// Decodes a data url into a Base64 image data and outs the media type.
+        /// </summary>
+        /// <param name="dataUrl">The data url to be decoded (e.g. data:image/png;base64,SGVsbG8gd29ybGQ=).</param>
+        /// <returns>A Base64 image data and outs the media type.</returns>
+        public static (string base64ImageData, string mediaType) Decode(this string dataUrl)
+        {
+            Match match = DataUrlRegex.Match(dataUrl);
+
+            if (!match.Success)
+            {
+                throw new ArgumentException($"Could not parse '{dataUrl}' as '\"data:(?<type>.+?),(?<data>.+)\"' data URL pattern.");
+            }
+
+            string mediaType = match.Groups["type"].Value;
+            string base64ImageData = match.Groups["data"].Value;
+
+            return (base64ImageData, mediaType);
+        }
+    }
+}
