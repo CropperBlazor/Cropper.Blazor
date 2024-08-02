7e73d559ba1adbe54a8f130e6bb5de3427580978
Gornytskyi Maxim <50423072+MaxymGorn@users.noreply.github.com>
2024-08-02 14:43:13 +0300

    Update Program.cs

--- src/Cropper.Blazor/Server/Program.cs
+++ src/Cropper.Blazor/Server/Program.cs
@@ -12,7 +12,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
 }
-else
+else 
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
