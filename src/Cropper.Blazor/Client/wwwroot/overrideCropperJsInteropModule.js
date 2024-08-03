1ab54729b41e3424fdd60123f6d466de93be5221
MaxymGorn <maximus56132@gmail.com>
2024-08-04 00:04:27 +0300

    fix some ESLINT errors

--- src/Cropper.Blazor/Client/wwwroot/overrideCropperJsInteropModule.js
+++ src/Cropper.Blazor/Client/wwwroot/overrideCropperJsInteropModule.js
@@ -1,15 +1,14 @@
-﻿window.overrideOnZoomCropperEvent = (minZoomRatio, maxZoomRatio) => {
+window.overrideOnZoomCropperEvent = (minZoomRatio, maxZoomRatio) => {
     window.cropper.onZoom = function (imageObject, event, correlationId) {
-		const jSEventData = this.getJSEventData(event, correlationId);
-		
-		const isApplyPreventZoomMinRatio = (minZoomRatio != null) && (minZoomRatio > event.detail.ratio);
-		const isApplyPreventZoomMaxRatio = (maxZoomRatio != null) && (event.detail.ratio > maxZoomRatio);
+        const jSEventData = this.getJSEventData(event, correlationId)
 
-		if (isApplyPreventZoomMinRatio || isApplyPreventZoomMaxRatio) {
-            event.preventDefault();
+        const isApplyPreventZoomMinRatio = (minZoomRatio != null) && (minZoomRatio > event.detail.ratio)
+        const isApplyPreventZoomMaxRatio = (maxZoomRatio != null) && (event.detail.ratio > maxZoomRatio)
+
+        if (isApplyPreventZoomMinRatio || isApplyPreventZoomMaxRatio) {
+            event.preventDefault()
+        } else {
+            imageObject.invokeMethodAsync('CropperIsZoomed', jSEventData)
         }
-        else {
-            imageObject.invokeMethodAsync('CropperIsZoomed', jSEventData);
-        }
-    };
-};
+    }
+}
--- src/Cropper.Blazor/Client/wwwroot/sw-registrator.js
+++ src/Cropper.Blazor/Client/wwwroot/sw-registrator.js
@@ -1,30 +1,30 @@
 window.updateAvailable = new Promise((resolve, reject) => {
-    if (!('serviceWorker' in navigator)) {
-        const errorMessage = `This browser doesn't support service workers`;
-        console.error(errorMessage);
-        reject(errorMessage);
-        return;
-    }
+  if (!('serviceWorker' in navigator)) {
+    const errorMessage = 'This browser doesn\'t support service workers'
+    console.error(errorMessage)
+    reject(errorMessage)
+    return
+  }
 
-    navigator.serviceWorker.register('/service-worker.min.js')
-        .then(registration => {
-            console.info(`Service worker registration successful (scope: ${registration.scope})`);
+  navigator.serviceWorker.register('/service-worker.min.js')
+    .then(registration => {
+      console.info(`Service worker registration successful (scope: ${registration.scope})`)
 
-            setInterval(() => {
-                registration.update();
-            }, 60 * 1000); // 60000ms -> check each minute
+      setInterval(() => {
+        registration.update()
+      }, 60 * 1000) // 60000ms -> check each minute
 
-            registration.onupdatefound = () => {
-                const installingServiceWorker = registration.installing;
-                installingServiceWorker.onstatechange = () => {
-                    if (installingServiceWorker.state === 'installed') {
-                        resolve(!!navigator.serviceWorker.controller);
-                    }
-                }
-            };
-        })
-        .catch(error => {
-            console.error('Service worker registration failed with error:', error);
-            reject(error);
-        });
-});
+      registration.onupdatefound = () => {
+        const installingServiceWorker = registration.installing
+        installingServiceWorker.onstatechange = () => {
+          if (installingServiceWorker.state === 'installed') {
+            resolve(!!navigator.serviceWorker.controller)
+          }
+        }
+      }
+    })
+    .catch(error => {
+      console.error('Service worker registration failed with error:', error)
+      reject(error)
+    })
+})
