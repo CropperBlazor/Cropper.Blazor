ec7d14c74f588f20a6cdd24de579f4e08d10cb6e
MaxymGorn <maximus56132@gmail.com>
2024-08-03 20:57:04 +0300

    revert js files

--- src/Cropper.Blazor/Client/wwwroot/jsObjectModule.js
+++ src/Cropper.Blazor/Client/wwwroot/jsObjectModule.js
@@ -1,33 +1,33 @@
 ﻿class JsObject {
-  getPropertyList(path) {
-    let res = path.replace("[", ".").replace("]", "").split(".");
+    getPropertyList(path) {
+        let res = path.replace('[', '.').replace(']', '').split('.');
 
-    if (res[0] === "") {
-      // if we pass "[0].id" we want to return [0,'id']
-      res.shift();
+        if (res[0] === "") { // if we pass "[0].id" we want to return [0,'id']
+            res.shift();
+        }
+
+        return res;
     }
 
-    return res;
-  }
+    getInstanceProperty(instance, propertyPath) {
 
-  getInstanceProperty(instance, propertyPath) {
-    if (propertyPath === "") {
-      return instance;
+        if (propertyPath === '') {
+            return instance;
+        }
+
+        let currentProperty = instance;
+        let splitProperty = this.getPropertyList(propertyPath);
+
+        for (let i = 0; i < splitProperty.length; i++) {
+            if (splitProperty[i] in currentProperty) {
+                currentProperty = currentProperty[splitProperty[i]];
+            } else {
+                return null;
+            }
+        }
+
+        return currentProperty;
     }
-
-    let currentProperty = instance;
-    let splitProperty = this.getPropertyList(propertyPath);
-
-    for (let i = 0; i < splitProperty.length; i++) {
-      if (splitProperty[i] in currentProperty) {
-        currentProperty = currentProperty[splitProperty[i]];
-      } else {
-        return null;
-      }
-    }
-
-    return currentProperty;
-  }
 }
 
 window.jsObject = new JsObject();
--- src/Cropper.Blazor/Client/wwwroot/overrideCropperJsInteropModule.js
+++ src/Cropper.Blazor/Client/wwwroot/overrideCropperJsInteropModule.js
@@ -1,16 +1,15 @@
 ﻿window.overrideOnZoomCropperEvent = (minZoomRatio, maxZoomRatio) => {
-  window.cropper.onZoom = function (imageObject, event, correlationId) {
-    const jSEventData = this.getJSEventData(event, correlationId);
+    window.cropper.onZoom = function (imageObject, event, correlationId) {
+		const jSEventData = this.getJSEventData(event, correlationId);
+		
+		const isApplyPreventZoomMinRatio = (minZoomRatio != null) && (minZoomRatio > event.detail.ratio);
+		const isApplyPreventZoomMaxRatio = (maxZoomRatio != null) && (event.detail.ratio > maxZoomRatio);
 
-    const isApplyPreventZoomMinRatio =
-      minZoomRatio != null && minZoomRatio > event.detail.ratio;
-    const isApplyPreventZoomMaxRatio =
-      maxZoomRatio != null && event.detail.ratio > maxZoomRatio;
-
-    if (isApplyPreventZoomMinRatio || isApplyPreventZoomMaxRatio) {
-      event.preventDefault();
-    } else {
-      imageObject.invokeMethodAsync("CropperIsZoomed", jSEventData);
-    }
-  };
+		if (isApplyPreventZoomMinRatio || isApplyPreventZoomMaxRatio) {
+            event.preventDefault();
+        }
+        else {
+            imageObject.invokeMethodAsync('CropperIsZoomed', jSEventData);
+        }
+    };
 };
