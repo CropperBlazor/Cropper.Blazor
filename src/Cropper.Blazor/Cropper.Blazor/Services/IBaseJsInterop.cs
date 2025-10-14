3e8253d6188e76a8887aafa6d455d57997014fcb
Maxym Gornytskiy <maximus56132@gmail.com>
2025-10-14 20:19:07 +0300

    fix

--- src/Cropper.Blazor/Cropper.Blazor/Services/IBaseJsInterop.cs
+++ src/Cropper.Blazor/Cropper.Blazor/Services/IBaseJsInterop.cs
@@ -15,11 +15,5 @@ namespace Cropper.Blazor.Services
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be canceled.</param>
         /// <returns>A <see cref="Task"/> representing any asynchronous operation.</returns>
         Task TryLoadModuleAsync(CancellationToken cancellationToken = default);
-
-        /// <summary>
-        /// Called to dispose this instance.
-        /// </summary>
-        /// <returns>A <see cref="ValueTask"/> representing any asynchronous operation.</returns>
-        ValueTask DisposeAsync();
     }
 }
