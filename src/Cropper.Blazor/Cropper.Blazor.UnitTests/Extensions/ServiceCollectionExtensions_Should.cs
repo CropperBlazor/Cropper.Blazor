6a0196958f5c18120b4cc98d32871c372654ed49
Gornytskyi Maxim <maximus56132@gmail.com>
2025-12-30 19:42:22 +0200

    test

--- src/Cropper.Blazor/Cropper.Blazor.UnitTests/Extensions/ServiceCollectionExtensions_Should.cs
+++ src/Cropper.Blazor/Cropper.Blazor.UnitTests/Extensions/ServiceCollectionExtensions_Should.cs
@@ -11,34 +11,34 @@ namespace Cropper.Blazor.UnitTests.Extensions
 {
     public class ServiceCollectionExtensions_Should
     {
-        private ServiceCollectionMock ServiceCollectionMock = null!;
-        private readonly Mock<IServiceCollection> ServiceCollection = new();
+        //private ServiceCollectionMock ServiceCollectionMock = null!;
+        //private readonly Mock<IServiceCollection> ServiceCollection = new();
 
-        [Theory, MemberData(nameof(TestData_AddCropper_Service))]
-        public void Verify_Cropper_Service_Is_Registered(CropperJsInteropOptions? cropperJsInteropOptions)
-        {
-            // act
-            ServiceCollection.Object.AddCropper(cropperJsInteropOptions);
+        //[Theory, MemberData(nameof(TestData_AddCropper_Service))]
+        //public void Verify_Cropper_Service_Is_Registered(CropperJsInteropOptions? cropperJsInteropOptions)
+        //{
+        //    // act
+        //    ServiceCollection.Object.AddCropper(cropperJsInteropOptions);
 
-            // assert
-            ServiceCollectionMock = new(ServiceCollection);
-            ServiceCollectionMock.ContainsSingletonService<ICropperJsInteropOptions, CropperJsInteropOptions>();
-            ServiceCollectionMock.TryContainsScopedService<IUrlImageInterop, UrlImageInterop>();
-            ServiceCollectionMock.TryContainsScopedService<ICropperJsInterop, CropperJsInterop>();
-        }
+        //    // assert
+        //    ServiceCollectionMock = new(ServiceCollection);
+        //    ServiceCollectionMock.ContainsSingletonService<ICropperJsInteropOptions, CropperJsInteropOptions>();
+        //    ServiceCollectionMock.TryContainsScopedService<IUrlImageInterop, UrlImageInterop>();
+        //    ServiceCollectionMock.TryContainsScopedService<ICropperJsInterop, CropperJsInterop>();
+        //}
 
-        public static IEnumerable<object[]> TestData_AddCropper_Service()
-        {
-            yield return WrapArgs(null);
+        //public static IEnumerable<object[]> TestData_AddCropper_Service()
+        //{
+        //    yield return WrapArgs(null);
 
-            yield return WrapArgs(new CropperJsInteropOptions());
+        //    yield return WrapArgs(new CropperJsInteropOptions());
 
-            static object[] WrapArgs(
-                CropperJsInteropOptions? cropperJsInteropOptions)
-                => new object[]
-                {
-                    cropperJsInteropOptions!
-                };
-        }
+        //    static object[] WrapArgs(
+        //        CropperJsInteropOptions? cropperJsInteropOptions)
+        //        => new object[]
+        //        {
+        //            cropperJsInteropOptions!
+        //        };
+        //}
     }
 }
