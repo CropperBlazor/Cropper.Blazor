85424bca83f50ca08f74a51d77cb86d294a4d375
Gornytskyi Maxim <maximus56132@gmail.com>
2026-01-30 01:19:49 +0200

    fix exception during prerender blazor server in dispose methods

--- src/Cropper.Blazor/Cropper.Blazor/Components/CropperComponent.razor.cs
+++ src/Cropper.Blazor/Cropper.Blazor/Components/CropperComponent.razor.cs
@@ -97,6 +97,8 @@ namespace Cropper.Blazor.Components
         [Parameter(CaptureUnmatchedValues = true)]
         public Dictionary<string, object> InputAttributes { get; set; } = null!;
 
+        private bool IsRendered = false;
+
         /// <summary>
         /// Method invoked after each time the component has been rendered. Note that the component does
         /// not automatically re-render after the completion of any returned <see cref="Task"/>, because
@@ -117,6 +119,8 @@ namespace Cropper.Blazor.Components
         {
             if (firstRender)
             {
+                IsRendered = true;
+
                 await CropperJsIntertop!.TryLoadModuleAsync();
             }
 
@@ -137,6 +141,11 @@ namespace Cropper.Blazor.Components
         /// <returns>A <see cref="ValueTask"/> representing any asynchronous operation.</returns>
         public async ValueTask DisposeAsync()
         {
+            if (!IsRendered && CropperJsIntertop.IsBlazorServer)
+            {
+                return;
+            }
+
             ElementReference? cropperElementReference = GetCropperElementReference();
 
             if (cropperElementReference.HasValue)
@@ -150,6 +159,11 @@ namespace Cropper.Blazor.Components
         /// </summary>
         public void Dispose()
         {
+            if (!IsRendered && CropperJsIntertop.IsBlazorServer)
+            {
+                return;
+            }
+
             ElementReference? cropperElementReference = GetCropperElementReference();
 
             if (cropperElementReference.HasValue)
